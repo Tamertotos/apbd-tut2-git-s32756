32a42665c345d423b456ae003ed97fd64fda2a49
Tamertotos <Tamercantotos@gmail.com>
2026-03-10 11:25:59 +0100

    Calculating average of numbers

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -5,3 +5,20 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+int[] arr = { 1, 2, 3, 4, 5 };
+Console.WriteLine(CalculateAverage(arr));
+
+
+static double CalculateAverage(int[] values)
+{
+    double average = 0.0;
+
+    for (int i = 0; i < values.Length; i++)
+    {
+        average += values[i];
+    }
+    
+    return average / values.Length;
+    
+}
